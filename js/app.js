30c23f8edc9311ced3f69bf5dcced705ccbc811e
Allan Crisanto do Carmo Fonseca <allan_crisanto@live.com>
2026-05-13 07:53:32 -0300

    melhoria tela inicial e cadastro

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
+    
     const btnLogout = document.getElementById('btn-logout');
     
     if (btnLogout) {
@@ -12,6 +13,69 @@ document.addEventListener('DOMContentLoaded', () => {
             }, 1500);
         });
     }
+
+    const registerForm = document.getElementById('register-form');
+
+    if (registerForm) {
+        registerForm.addEventListener('submit', (e) => {
+            e.preventDefault();
+            
+            const nome = document.getElementById('reg-nome').value;
+            const email = document.getElementById('reg-email').value;
+            const senha = document.getElementById('reg-senha').value;
+            const confirmar = document.getElementById('reg-confirmar-senha').value;
+
+            if (senha !== confirmar) {
+                showToast('As senhas não coincidem! 🌵', 'warning');
+                return;
+            }
+
+            let usuarios = JSON.parse(localStorage.getItem('raizes_users')) || [];
+            
+            const usuarioExiste = usuarios.find(u => u.email === email);
+            if (usuarioExiste) {
+                showToast('Este e-mail já está cadastrado!', 'error');
+                return;
+            }
+
+            const novoUsuario = { nome, email, senha, points: 0 };
+            usuarios.push(novoUsuario);
+            localStorage.setItem('raizes_users', JSON.stringify(usuarios));
+
+            showToast('Cadastro realizado! Redirecionando...', 'success');
+            
+            setTimeout(() => {
+                window.location.href = 'index.html';
+            }, 2000);
+        });
+    }
+    const loginForm = document.getElementById('login-form');
+
+    if (loginForm) {
+        loginForm.addEventListener('submit', (e) => {
+            e.preventDefault();
+
+            const emailInput = document.getElementById('email').value;
+            const senhaInput = document.getElementById('senha').value;
+
+            let usuarios = JSON.parse(localStorage.getItem('raizes_users')) || [];
+
+            const usuarioValido = usuarios.find(u => u.email === emailInput && u.senha === senhaInput);
+
+            if (usuarioValido) {
+                localStorage.setItem('raizes_currentUser', JSON.stringify(usuarioValido));
+                
+                showToast(`Bem-vindo de volta, ${usuarioValido.nome}! 🌵`, 'success');
+                
+                setTimeout(() => {
+                    window.location.href = 'unidades.html'; 
+                }, 1500);
+            } else {
+                showToast('Ops! E-mail ou senha incorretos.', 'error');
+            }
+        });
+    }
+
 });
 
 window.showToast = function(message, type = 'success') {
